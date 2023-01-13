b116526d923ed22e5b0ce2568344b6d46fb84851
Joe Webb <webbdeveloping@gmail.com>
2023-01-13 15:12:00 -0700

    Converted page from Webflow to Next.js
    I still need to add animations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "../styles/css/normalize.css";
+import "../styles/css/webflow.css";
+import "../styles/css/robdoesmedicare.webflow.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
